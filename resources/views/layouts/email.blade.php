dce60eda0cf823a3f38cae4fc3f647511e23e79d
Aman Saini <amansaini87@rediffmail.com>
2023-06-23 19:53:21 +0530

    added background in template

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html
-  PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
-  <title>@yield('title') | {{ env('WEBSITE_TITLE') }}</title>
+  <title></title>
   <meta name="viewport" content="width=device-width" />
   <!-- Favicon icon -->
   <link rel="icon" href="../email-templates/img/favicon.ico" type="image/x-icon">
@@ -44,50 +43,41 @@
           <tr>
             <td>
         <![endif]-->
-  <table align="center" border="0" cellpadding="0" cellspacing="0"
-    style="border-collapse: collapse; width: 100%; max-width: 600px;" class="content">
-    <tr>
-      <td style="padding: 15px 10px 15px 10px;">
-        <table border="0" cellpadding="0" cellspacing="0" width="100%">
-          <tr>
-            <td align="center" style="color: #fff; font-family: Arial, sans-serif; font-size: 12px;">
-              Email not displaying correctly? <a href="#" style="color: #0073AA;">View it in your browser</a>
-            </td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-    <tr>
-      <td align="center" bgcolor="#0073AA"
-        style="padding: 20px 20px 20px 20px; color: #ffffff; font-family: Arial, sans-serif; font-size: 36px; font-weight: bold;">
-        <img src="{{ asset('images/logo.png') }}" alt="{{ env('WEBSITE_TITLE') }} Logo" width="175"
-          style="display:block;" />
-      </td>
-    </tr>
-    @yield('body')
-    <tr>
-      <td align="center" bgcolor="#dddddd"
-        style="padding: 15px 10px 15px 10px; color: #555555; font-family: Arial, sans-serif; font-size: 12px; line-height: 18px;">
-        <b>Company Inc.</b><br />985 Example St. &bull; Suite A1S2 &bull; San Francisco, CA 12458 USA
-      </td>
-    </tr>
-    <tr>
-      <td style="padding: 15px 10px 15px 10px;">
-        <table border="0" cellpadding="0" cellspacing="0" width="100%">
-          <tr>
-            <td align="center" width="100%" style="color: #fff; font-family: Arial, sans-serif; font-size: 12px;">
-              {{ date('Y') }} &copy; <a href="{{ route('home') }}" style="color: #0073AA;">{{ env('WEBSITE_TITLE') }}</a>
-            </td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-  </table>
+  <div style="background: #34495E">
+    <table align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse; width: 100%; max-width: 600px;" class="content">
+      <tr>
+        <td align="center" bgcolor="#0073AA"
+          style="padding: 20px 20px 20px 20px; color: #ffffff; font-family: Arial, sans-serif; font-size: 36px; font-weight: bold;">
+          <img src="{{ asset('images/logo.png') }}" alt="{{ env('WEBSITE_TITLE') }} Logo" width="200" style="display:block;" />
+        </td>
+      </tr>
+
+      @yield('body')
+
+      <tr>
+        <td align="center" bgcolor="#dddddd"
+          style="padding: 15px 10px 15px 10px; color: #555555; font-family: Arial, sans-serif; font-size: 12px; line-height: 18px;">
+          <b>Company Inc.</b><br />985 Example St. &bull; Suite A1S2 &bull; San Francisco, CA 12458 USA
+        </td>
+      </tr>
+      <tr>
+        <td style="padding: 15px 10px 15px 10px;">
+          <table border="0" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+              <td align="center" width="100%" style="color: #fff; font-family: Arial, sans-serif; font-size: 12px;">
+                {{ date('Y') }} &copy; <a href="{{ route('home') }}" style="color: #0073AA;">{{ env('WEBSITE_TITLE') }}</a>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
   <!--[if (gte mso 9)|(IE)]>
                 </td>
             </tr>
         </table>
         <![endif]-->
+  </div>
 </body>
 
 </html>
